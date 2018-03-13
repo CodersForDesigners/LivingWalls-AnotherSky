75aae93d8c8af1dd5aacd4034feb500600f8130b
adi <adityabhat@lazaro.in>
2018-03-13 11:47:19 +0530

    Apartment Pricing Page; rename it so it does not get accidently navigated to
    This happens now that we're dynamically changing the URLs to reflect the section
    that is currently being viewed. When the "Apartment-Pricing" section is viewed,
    the URL becomes /apartment-pricing . Then if the page is refreshed, it actually
    goes to the old "Apartment Pricing" page.

--- pages/unused/apartment-pricing.php
+++ pages/unused/apartment-pricing.php
@@ -87,7 +87,47 @@
 	<link rel="stylesheet" type="text/css" href="css/pages/pricing.css">
 
 	<!-- Analytics -->
-	<!-- redo the Google Analytics, Facebook Pixel code -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-57124254-5', 'auto');
+	  ga('send', 'pageview');
+	</script>
+	<!-- End of Google Analytics -->
+
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+		fbq('init', '1003566166360897');
+		fbq('track', "PageView");
+	</script>
+	<noscript>
+		<img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=1003566166360897&ev=PageView&noscript=1"/>
+	</noscript>
+	<!-- End Facebook Pixel Code -->
+
+	<!-- Facebook Conversion Code for Leads - Living Walls Advertising -->
+	<script>(function() {
+	  var _fbq = window._fbq || (window._fbq = []);
+	  if (!_fbq.loaded) {
+	    var fbds = document.createElement('script');
+	    fbds.async = true;
+	    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(fbds, s);
+	    _fbq.loaded = true;
+	  }
+		})();
+		window._fbq = window._fbq || [];
+	</script>
 
 </head>
 
@@ -542,7 +582,7 @@
 <!-- jQuery -->
 <script type="text/javascript" src="js/jquery-min.js"></script>
 
-<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+<!-- <script type="text/javascript" src="js/jquery-ui.min.js"></script> -->
 <script type="text/javascript" src="js/jquery.visible.min.js"></script>
 <script type="text/javascript" src="js/modules/scrollSmooth.js"></script>
 <script type="text/javascript" src="js/ion.rangeSlider.min.js"></script>
