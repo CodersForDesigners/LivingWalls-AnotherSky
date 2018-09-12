1d46ba159b13c14ebf027d852806bd4f9196a1d6
adi <adityabhat@lazaro.in>
2018-09-12 13:04:55 +0530

    Deployment : Update the post-deploy script; Add folder creation and permissions

--- post-deploy.sh
+++ post-deploy.sh
@@ -11,6 +11,9 @@ done
 
 # Establish symbolic links to the `media` and `enquiries` directories
 rm media
+mkdir -p ../media/${PROJECT_DIR}
 ln -s ../media/${PROJECT_DIR} media
 rm enquiries
+mkdir -p ../enquiries/${PROJECT_DIR}/{customer,salesperson}
+chmod -R 757 ../enquiries/${PROJECT_DIR}
 ln -s ../enquiries/${PROJECT_DIR} enquiries
